4dbdd921827223b9e42d748a424eac66c62ab9a1
Magdalena Grad <mgdln.grad@gmail.com>
2018-07-29 20:52:22 +0200

    add zadanie

--- js/Board.js
+++ js/Board.js
@@ -2,9 +2,9 @@ var board = {
     name: 'Kanban Board',
     addColumn: function(column) {
         this.element.appendChild(column.element);
-        initSortable(column.id); //About this feature we will tell later
+        initSortable(column.id);
     },
-    element: document.querySelector('#board .scrolling-wrapper-flexbox') //replace .column-container
+    element: document.querySelector('#board .column-container')
 };
 function initSortable(id) {
     var el = document.getElementById(id);
@@ -33,42 +33,3 @@ document.querySelector('#board .create-column').addEventListener('click', functi
         board.addColumn(column);
     });
 });
-
-// var board = {
-//     name: 'Tablica Kanban',
-//     addColumn: function(column) {
-//       this.element.appendChild(column.element);
-//       initSortable(column.id); //About this feature we will tell later
-//     },
-//     element: document.querySelector('#board .column-container')
-// };
-
-// document.querySelector('#board .create-column').addEventListener('click', function() {
-//     var name = prompt('Enter a column name');
-//     // var column = new Column(name);
-//     // board.addColumn(column);
-//     var data = new FormData();
-
-//   data.append('name', name);
-
-//   fetch(baseUrl + '/column', {
-//       method: 'POST',
-//       headers: myHeaders,
-//       body: data,
-//     })
-//     .then(function(resp) {
-//       return resp.json();
-//     })
-//     .then(function(resp) {
-//       var column = new Column(resp.id, name);
-//       board.addColumn(column);
-//     });
-// });
-	
-// function initSortable(id) {
-//   	var el = document.getElementById(id);
-//   	var sortable = Sortable.create(el, {
-//     	group: 'kanban',
-//     	sort: true
-//   	});
-// }
